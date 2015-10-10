b4eef24bdfee404224faf18683c63c2cadd13a12
skippyth <kbigelow@berkeley.edu>
2015-10-10 12:03:03 -0700

    Added a checkbox for "pause after this song."

--- options.js
+++ options.js
@@ -6,10 +6,15 @@ $('#autoPauseYouTube').change(function () {
   localStorage["autoPauseYouTube"] = $(this).prop('checked');
 });
 
+$('#togglePlayNext').change(function () {
+	localStorage["togglePlayNext"] = $(this).prop('checked');
+})
+
 // Restores select box state to saved value from localStorage.
 function restore_options() {
   $('#notifications').prop('checked', parseBool(localStorage["showNotifications"]));
   $('#autoPauseYouTube').prop('checked', parseBool(localStorage["autoPauseYouTube"]));
-}
+  $('#togglePlayNext').prop('checked', parseBool(localStorage["togglePlayNext"]));
+  }
 
 $(document).ready(restore_options);
