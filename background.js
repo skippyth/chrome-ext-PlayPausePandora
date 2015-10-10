b816e13e6abadbdeb396f640acdb753645db0fd4
skippyth <kbigelow@berkeley.edu>
2015-10-10 11:59:53 -0700

    Added "pause after this song" functionality.

--- background.js
+++ background.js
@@ -4,233 +4,311 @@ var isPlaying = false;
 var wasPlaying = false;
 var alreadyClicked = false;
 var DEBUG = false;
+var pauseNext = false;
+var skip = false;
 
 var pandoraUrl = "http://www.pandora.com/";
 
 function isPandoraUrl(url) {
-  // Return whether the URL starts with the Pandora prefix.
-  return url.indexOf(pandoraUrl) == 0;
+	// Return whether the URL starts with the Pandora prefix.
+	return url.indexOf(pandoraUrl) == 0;
 }
 
 function onInit() {
-  if (DEBUG)
-  {
-    console.log("onInit");
-  }
-  if (pandoraTabId != null) {
-    chrome.tabs.executeScript(pandoraTabId, {
-      code: 'var pauseButton = document.getElementsByClassName("pauseButton")[0]; \
-             var playButton = document.getElementsByClassName("playButton")[0]; \
-             if (playButton.style.display == "none" ) { "playing"; } \
-             else { "paused"; }'
-    }, function (callback) {
-      if (callback == "playing") {
-        chrome.browserAction.setIcon({path:"action-pause.png"});
-        isPlaying = true;
-      } else if (callback == "paused") {
-        chrome.browserAction.setIcon({path:"action-play.png"});
-        isPlaying = false;
-      }
-    });
-  }
+	if (DEBUG) {
+		console.log("onInit");
+	}
+	if (pandoraTabId != null) {
+		chrome.tabs.executeScript(pandoraTabId, {
+			code : 'var pauseButton = document.getElementsByClassName("pauseButton")[0]; \
+									var playButton = document.getElementsByClassName("playButton")[0]; \
+									if (playButton.style.display == "none" ) { "playing"; } \
+									else { "paused"; }'
+		}, function (callback) {
+			if (callback == "playing") {
+				chrome.browserAction.setIcon({
+					path : "action-pause.png"
+				});
+				isPlaying = true;
+			} else if (callback == "paused") {
+				chrome.browserAction.setIcon({
+					path : "action-play.png"
+				});
+				isPlaying = false;
+			}
+		});
+	}
 }
 
 function checkIfPandoraHasScripts() {
-  chrome.tabs.sendMessage(pandoraTabId, {greeting: "hello"}, function(response) {
-    if (response) {
-        if (DEBUG)
-        {
-          console.log("Already there");
-        }
-    }
-    else {
-        if (DEBUG)
-        {
-          console.log("Content scripts not there, injecting content scripts");
-        }
-        chrome.tabs.executeScript(pandoraTabId, {file: "jquery.min.js"});
-        chrome.tabs.executeScript(pandoraTabId, {file: "pppandora.js"});
-        chrome.tabs.executeScript(pandoraTabId, {file: "areyoulistening_loop.js"});
-        chrome.tabs.executeScript(pandoraTabId, {code: "getPlaying(); getSongInfo();"});
-    }
-  });
+	chrome.tabs.sendMessage(pandoraTabId, {
+		greeting : "hello"
+	}, function (response) {
+		if (response) {
+			if (DEBUG) {
+				console.log("Already there");
+			}
+		} else {
+			if (DEBUG) {
+				console.log("Content scripts not there, injecting content scripts");
+			}
+			chrome.tabs.executeScript(pandoraTabId, {
+				file : "jquery.min.js"
+			});
+			chrome.tabs.executeScript(pandoraTabId, {
+				file : "pppandora.js"
+			});
+			chrome.tabs.executeScript(pandoraTabId, {
+				file : "areyoulistening_loop.js"
+			});
+			chrome.tabs.executeScript(pandoraTabId, {
+				code : "getPlaying(); getSongInfo();"
+			});
+		}
+	});
 }
 
 function pandoraTabRemoved(tabId, oRemoveInfo) {
-  if (DEBUG)
-  {
-    console.log(tabId + " " + pandoraTabId);
-  }
-  if (tabId == pandoraTabId) {
-    if (DEBUG)
-    {
-      console.log("Pandora tab closed! Nooooooooooooooo!");
-    }
-    chrome.browserAction.setIcon({path:"action-play.png"});
-    isPlaying = false;
-    pandoraTabId = null;
-  }
+	if (DEBUG) {
+		console.log(tabId + " " + pandoraTabId);
+	}
+	if (tabId == pandoraTabId) {
+		if (DEBUG) {
+			console.log("Pandora tab closed! Nooooooooooooooo!");
+		}
+		chrome.browserAction.setIcon({
+			path : "action-play.png"
+		});
+		isPlaying = false;
+		pandoraTabId = null;
+	}
 }
 
 function getAllWindows() {
-  chrome.windows.getAll({populate:true},function (windows) {
-    for (var i = 0; i < windows.length; i++) {
-      getPandoraTabId(windows[i].id);
-    };
-  });
+	chrome.windows.getAll({
+		populate : true
+	}, function (windows) {
+		for (var i = 0; i < windows.length; i++) {
+			getPandoraTabId(windows[i].id);
+		};
+	});
 }
 
 function getPandoraTabId(windowId) {
-  chrome.tabs.getAllInWindow(windowId, function(tabs) {
-    for (var i = 0, tab; tab = tabs[i]; i++) {
-      if (tab.url && isPandoraUrl(tab.url)) {
-        if (DEBUG)
-        {
-          console.log("Found tab id: " + tab.id + " in window: " + windowId);
-        }
-        pandoraTabId = tab.id;
-        pandoraWindowId = window.id;
-        onPandoraTabFound();
-        checkIfPandoraHasScripts();
-      }
-    }
-  });
+	chrome.tabs.getAllInWindow(windowId, function (tabs) {
+		for (var i = 0, tab; tab = tabs[i]; i++) {
+			if (tab.url && isPandoraUrl(tab.url)) {
+				if (DEBUG) {
+					console.log("Found tab id: " + tab.id + " in window: " + windowId);
+				}
+				pandoraTabId = tab.id;
+				pandoraWindowId = window.id;
+				onPandoraTabFound();
+				checkIfPandoraHasScripts();
+			}
+		}
+	});
 }
 
 function onPandoraTabFound() {
-  chrome.browserAction.setIcon({path:"action-pause.png"});
-  isPlaying = true;
+	chrome.browserAction.setIcon({
+		path : "action-pause.png"
+	});
+	isPlaying = true;
 }
 
 function goToPandora() {
-  if (alreadyClicked && pandoraTabId != null) {
-    //Yes, Previous Click Detected
+	if (alreadyClicked && pandoraTabId != null) {
+		//Yes, Previous Click Detected
 
-    //Clear timer already set in earlier Click
-    clearTimeout(timer);
-    if (DEBUG)
-    {
-      console.log("Double click - skipping song");
-    }
+		//Clear timer already set in earlier Click
+		clearTimeout(timer);
+		if (DEBUG) {
+			console.log("Double click - skipping song");
+		}
+		// checks if it's pause or skip
+		// gets to skip after double clicking the pauseNext button
+		if (pauseNext) {
+			skip = true;
+			pauseNext = false;
+		}
+		if (skip||!parseBool(localStorage["togglePlayNext"])) {
+			chrome.browserAction.setIcon({
+				path : "action-pause.png"
+			});
+			chrome.tabs.executeScript(pandoraTabId, {
+				code : "$('.skipButton').click();"
+			});
+			skip = false;
+		}
 
-    chrome.tabs.executeScript(pandoraTabId, {
-      code: "$('.skipButton').click();"
-    });
+		//triggers pausenext instead of skipping
+		else if (parseBool(localStorage["togglePlayNext"])) {
+			chrome.browserAction.setIcon({
+				path : "action-pause-next.png"
+			});
+			pauseNext = true;
+		}
+		//Clear all Clicks
+		alreadyClicked = false;
+		return;
+	}
 
-    //Clear all Clicks
-    alreadyClicked = false;
-    return;
-  }
+	alreadyClicked = true;
 
-  alreadyClicked = true;
+	//Add a timer to detect next click to a sample of 250
+	timer = setTimeout(function () {
+			//No more clicks so, this is a single click
+			if (DEBUG) {
+				console.log("Single click");
+			}
 
-  //Add a timer to detect next click to a sample of 250
-  timer = setTimeout(function () {
-    //No more clicks so, this is a single click
-    if (DEBUG)
-    {
-      console.log("Single click");
-    }
+			if (DEBUG) {
+				console.log('Going to pandora...');
+			}
 
-    if (DEBUG)
-    {
-      console.log('Going to pandora...');
-    }
+			if (pandoraTabId != null) {
+				chrome.tabs.executeScript(pandoraTabId, {
+					code : "$('.pauseButton:visible, .playButton:visible').click();"
+				});
+			} else {
+				chrome.browserAction.setIcon({
+					path : "action-pause.png"
+				});
+				isPlaying = true;
+				chrome.tabs.create({
+					url : pandoraUrl
+				});
+				getAllWindows();
+			}
 
-    if (pandoraTabId != null)
-    {
-      chrome.tabs.executeScript(pandoraTabId, {
-        code: "$('.pauseButton:visible, .playButton:visible').click();"
-      });
-    }
-    else
-    {
-      chrome.browserAction.setIcon({path:"action-pause.png"});
-      isPlaying = true;
-      chrome.tabs.create({url: pandoraUrl});
-      getAllWindows();
-    }
+			//Clear all timers
+			clearTimeout(timer);
 
-    //Clear all timers
-    clearTimeout(timer);
-
-    //Ignore clicks
-    alreadyClicked = false;
-  }, 250);
+			//Ignore clicks
+			alreadyClicked = false;
+		}, 250);
 }
 
 function onMessage(request, sender, sendResponse) {
-  switch (request.message) {
-    case "paused":
-      chrome.browserAction.setIcon({path:"action-play.png"});
-      isPlaying = false;
-      break;
-    case "playing":
-      chrome.browserAction.setIcon({path:"action-pause.png"});
-      isPlaying = true;
-      break;
-    case "songChanged":
-      chrome.browserAction.setTitle({ title: "Song: " + request.songTitle + "\nArtist: " + request.songArtist + "\nAlbum: " + request.songAlbum });
-      if (parseBool(localStorage["showNotifications"])) {
-        var options = {
-          type: "list",
-          title: request.songTitle,
-          message: "",
-          iconUrl: request.songArt,
-          items: [
-            { title: "Artist", message: request.songArtist},
-            { title: "Album", message: request.songAlbum}
-          ]
-        };
-        chrome.notifications.create(options.title, options, function () {});
-        setTimeout(function() { chrome.notifications.clear(options.title, function() {}); }, 5000);
-      }
-      break;
-    case "youtube_playing":
-      wasPlaying = isPlaying;
-      if (parseBool(localStorage["autoPauseYouTube"])) {
-        chrome.tabs.executeScript(pandoraTabId, {
-          code: "$('.pauseButton:visible').click();"
-        });
-      }
-      break;
-    case "youtube_paused":
-      if (parseBool(localStorage["autoPauseYouTube"]) && wasPlaying) {
-        chrome.tabs.executeScript(pandoraTabId, {
-          code: "$('.playButton:visible').click();"
-        });
-      }
-      break;
-  }
+	switch (request.message) {
+	case "paused":
+		//checks which icon it should use
+		if (pauseNext) {
+			chrome.browserAction.setIcon({
+				path : "action-play-next.png"
+			});
+		} else {
+			chrome.browserAction.setIcon({
+				path : "action-play.png"
+			});
+		}
+		isPlaying = false;
+		break;
+	case "playing":
+	//checks which icon it should use
+		if (pauseNext) {
+			chrome.browserAction.setIcon({
+				path : "action-pause-next.png"
+			});
+		} else {
+			chrome.browserAction.setIcon({
+				path : "action-pause.png"
+			});
+		}
+		isPlaying = true;
+		break;
+	case "songChanged":
+		chrome.browserAction.setTitle({
+			title : "Song: " + request.songTitle + "\nArtist: " + request.songArtist + "\nAlbum: " + request.songAlbum
+		});
+		if (parseBool(localStorage["showNotifications"])) {
+			var options = {
+				type : "list",
+				title : request.songTitle,
+				message : "",
+				iconUrl : request.songArt,
+				items : [{
+						title : "Artist",
+						message : request.songArtist
+					}, {
+						title : "Album",
+						message : request.songAlbum
+					}
+				]
+			};
+			chrome.notifications.create(options.title, options, function () {});
+			setTimeout(function () {
+				chrome.notifications.clear(options.title, function () {});
+			}, 5000);
+		}
+		if (pauseNext) {
+			pauseNext = false;
+			if (pandoraTabId != null) {
+				chrome.tabs.executeScript(pandoraTabId, {
+					code : "$('.pauseButton:visible, .playButton:visible').click();"
+				});
+			} else {
+				chrome.browserAction.setIcon({
+					path : "action-pause.png"
+				});
+				isPlaying = true;
+				chrome.tabs.create({
+					url : pandoraUrl
+				});
+				getAllWindows();
+			}
+		}
+
+		break;
+	case "youtube_playing":
+		wasPlaying = isPlaying;
+		if (parseBool(localStorage["autoPauseYouTube"])) {
+			chrome.tabs.executeScript(pandoraTabId, {
+				code : "$('.pauseButton:visible').click();"
+			});
+		}
+		break;
+	case "youtube_paused":
+		if (parseBool(localStorage["autoPauseYouTube"]) && wasPlaying) {
+			chrome.tabs.executeScript(pandoraTabId, {
+				code : "$('.playButton:visible').click();"
+			});
+		}
+		break;
+	}
 }
 
 function onCreated(tab) {
-  if (DEBUG)
-  {
-    console.log("Tab Created", tab);
-  }
-  if (isPandoraUrl(tab.url)) {
-    chrome.browserAction.setIcon({path:"action-pause.png"});
-    isPlaying = true;
-    pandoraTabId = tab.id;
-  }
+	if (DEBUG) {
+		console.log("Tab Created", tab);
+	}
+	if (isPandoraUrl(tab.url)) {
+		chrome.browserAction.setIcon({
+			path : "action-pause.png"
+		});
+		isPlaying = true;
+		pandoraTabId = tab.id;
+	}
 }
 
 function onUpdated(tabId, oChangeInfo, tab) {
-  if (DEBUG)
-  {
-    console.log("Tab Updated", tab);
-  }
-  if (tab.status == "complete" && isPandoraUrl(tab.url)) {
-    chrome.browserAction.setIcon({ path:"action-pause.png" });
-    isPlaying = true;
-    pandoraTabId = tab.id;
-  }
-  else if (tab.id == pandoraTabId && !isPandoraUrl(tab.url)) {
-    chrome.browserAction.setIcon({ path:"action-play.png" });
-    isPlaying = false;
-    pandoraTabId = null;
-  }
+	if (DEBUG) {
+		console.log("Tab Updated", tab);
+	}
+	if (tab.status == "complete" && isPandoraUrl(tab.url)) {
+		chrome.browserAction.setIcon({
+			path : "action-pause.png"
+		});
+		isPlaying = true;
+		pandoraTabId = tab.id;
+	} else if (tab.id == pandoraTabId && !isPandoraUrl(tab.url)) {
+		chrome.browserAction.setIcon({
+			path : "action-play.png"
+		});
+		isPlaying = false;
+		pandoraTabId = null;
+	}
 }
 
 getAllWindows();
